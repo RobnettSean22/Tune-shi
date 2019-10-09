eba8b1d419b47d72b74db93c9bfef5f5ebd3e88a
RobnettSean22 <robnettsean22@gmail.com>
2019-10-08 23:11:32 -0700

    Initial commit post component/server

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+import Post from './component/Post'
+
+ class App extends Component{
+   
+   render(){
+
+   
+  return (
+    <div className="profile">
+      <Post/>
+    </div>
+ 
+    );
+  }
+}
+
+
+export default App;
---
+++ src/component/Post.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+export default class Post extends Component{
+    constructor(){
+        super()
+        this.state = {
+            postBoard:[],
+            newPost:''
+        }
+        this.handleAddPost = this.handleAddPost.bind(this)
+        this.handlePostBoard = this.handlePostBoard.bind(this)
+    }
+    handleAddPost(value){
+        this.setState({
+            newPost:value
+        })
+    }
+
+    handlePostBoard(index){
+        const message = this.state.newPost
+        this.setState({
+            //when pushing an element into the array in react use the method bellow the spread operator (...) makes a copy of the arrray after the comma but in what you want to add!!!!
+            postBoard: [...this.state.postBoard, message],
+            newPost:''
+
+        })
+
+    }
+
+    render(){
+        let {postBoard} = this.state
+        let updatePost = postBoard.map((element, index) =>{
+            return(
+                <div key={index}>
+                    <p>{element}</p>
+                </div>
+            )
+        })
+        return(
+            <div className='Posts'>
+                <textarea value={this.state.newPost} placeholder='Whachu got' 
+                 onChange={(e) => {this.handleAddPost(e.target.value)}}></textarea>
+                <button onClick={(e) => this.handlePostBoard(e.target.value)}>Send that ish</button>
+                <div className='messaages'>
+                    {updatePost}
+                </div>
+            </div>
+
+        )
+    }
+}
